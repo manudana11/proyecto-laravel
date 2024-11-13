6326fd816e961b55630c0da8eb2ac4f3b39d4d2e
manudana11 <manueldanapallardo@gmail.com>
2024-11-13 15:06:52 +0100

    final

--- laravel/resources/views/components/layout.blade.php
+++ laravel/resources/views/components/layout.blade.php
@@ -9,8 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @routes
+    @Vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-black text-white font-hanken-grotesk pb-20">
     <div class="px-10">
